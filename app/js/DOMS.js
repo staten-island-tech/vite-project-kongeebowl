272c2576f0e70f304c87b1e19e7abd5ad89ae714
kongeebowl <kadenp11@nycstudents.net>
2024-11-08 12:28:40 -0500

    e

--- app/js/DOMS.js
+++ app/js/DOMS.js
@@ -1,6 +1,10 @@
 const DOMSelectors = {
   box: document.getElementById("container"),
   button: document.getElementById("btn"),
+  rizz: document.getElementById("rizzbtn"),
+  hot: document.getElementById("hotbtn"),
+  wiz: document.getElementById("wizbtn"),
+  swol: document.getElementById("swolbtn"),
 };
 
 export { DOMSelectors };
--- app/js/main.js
+++ app/js/main.js
@@ -2,25 +2,58 @@ import "../css/style.css";
 import { gnomes } from "./products";
 import { DOMSelectors } from "./DOMS";
 
-function rizzButton() {
-  DOMSelectors.button;
-}
-
-function addCards() {
-  gnomes.forEach((gnome) =>
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      `<div class="card"> 
+function addCards(gnome) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card"> 
         <h2>${gnome.name}</h2>
         <img src="${gnome.image}" alt="" class="card-img">
         <p>${gnome.desc}</p>
-      </div>
-      `
-    )
+      </div>`
   );
 }
-addCards();
 
-function rizzGods() {
-  gnomes.filter((gnome) => gnome.rizz >= 7);
+function clear() {
+  DOMSelectors.box.innerHTML = "";
 }
+
+function filterAndSortGnomes(trait, threshold) {
+  return gnomes
+    .filter((gnome) => gnome[trait] >= threshold) // Filter gnomes based on the threshold
+    .sort((a, b) => b[trait] - a[trait]); // Sort the filtered gnomes by the trait in descending order
+}
+
+function gnomeCards(filteredGnomes) {
+  clear();
+  filteredGnomes.forEach((gnome) => addCards(gnome));
+}
+
+function displayCards() {
+  gnomeCards(gnomes);
+
+  DOMSelectors.rizz.addEventListener("click", (event) => {
+    event.preventDefault();
+    const rizzLords = filterAndSortGnomes("rizz", 7); // Filter and sort by rizz
+    gnomeCards(rizzLords);
+  });
+
+  DOMSelectors.hot.addEventListener("click", (event) => {
+    event.preventDefault();
+    const looksmaxxer = filterAndSortGnomes("looks", 8); // Filter and sort by looks
+    gnomeCards(looksmaxxer);
+  });
+
+  DOMSelectors.wiz.addEventListener("click", (event) => {
+    event.preventDefault();
+    const wizard = filterAndSortGnomes("magic", 8); // Filter and sort by magic
+    gnomeCards(wizard);
+  });
+
+  DOMSelectors.swol.addEventListener("click", (event) => {
+    event.preventDefault();
+    const beachBum = filterAndSortGnomes("strength", 9); // Filter and sort by strength
+    gnomeCards(beachBum);
+  });
+}
+
+displayCards();
